c5906ab23e471ea16cd41a275493179d5e659336
Mark May <mamark90@gmail.com>
2019-06-26 22:57:49 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,15 +1,16 @@
-require_relative './lib student.rb'
+require 'pry'
+require_relative 'student.rb'
 
 class ChattyStudent < Student
 
-  def hello
-    do super.10 times
-      puts "How are you doing today? I'm okay, but I'm
-      kind of tired. Did you watch The Walking Dead
-      last night? You didn't?! Oh man, it was so crazy!
-       What, you don't want any spoilers? Okay well let
-      me just tell you who died..."
+    def hello
+      super
+        puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
     end
-  end
 
+    def raise_hand
+      super
+        10.times {"#{raise_hand}"}
+
+    end
 end
---
+++ lib/student.rb
@@ -0,0 +1,11 @@
+class Student
+  attr_accessor :pick_me
+
+  def hello
+    puts "Hey there! I'm so excited to learn stuff."
+  end
+
+  def raise_hand
+    puts "Pick me!"
+  end
+end
